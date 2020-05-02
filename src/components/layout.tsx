8e44bdf4dceb867d53d958637105e8a4dd9daca4
alescandone <alescandone@gmail.com>
2020-05-02 16:16:29 +0200

    refactored footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent, useRef, useEffect, useState } from 'react'
-import { Footer } from './footer'
 import './layout.css'
 import Header from './header'
-import { Helmet } from 'react-helmet'
 import Contatti from './contatti'
 
 const Layout: FunctionComponent<{}> = ({ children }) => {
@@ -16,18 +14,11 @@ const Layout: FunctionComponent<{}> = ({ children }) => {
 
   return (
     <div className="h-screen flex flex-col">
-      {/* <Helmet>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet> */}
-
       <Header darkBg={darkBg} />
       <main className="flex-1">{children}</main>
-      <div ref={contacts}>
+      <footer ref={contacts}>
         <Contatti />
-      </div>
+      </footer>
     </div>
   )
 }
