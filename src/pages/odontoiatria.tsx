5e3c7959a672adc250308181572fbfd6edde42e8
alescandone <alescandone@gmail.com>
2020-05-09 15:14:08 +0200

    removed description

--- src/pages/odontoiatria.tsx
+++ src/pages/odontoiatria.tsx
@@ -3,11 +3,7 @@ import Services from '../components/services'
 import data from '../data/servizi-odontoiatria'
 
 const Odontoiatria: FunctionComponent = () => (
-  <Services
-    header="Odontoiatria"
-    services={data}
-    descr="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum aspernatur nemo, voluptate praesentium magni enim, reprehenderit, facilis ipsa quo labore odit ducimus quidem deserunt minus unde quos. Dolor, fugiat dolore."
-  />
+  <Services header="Odontoiatria" services={data} />
 )
 
 export default Odontoiatria
