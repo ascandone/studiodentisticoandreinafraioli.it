67551bcdf09a3db1b87f26416ef6d9be2dedfa1a
alescandone <alescandone@gmail.com>
2020-10-21 14:38:21 +0200

    small design updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 const colors = require('@tailwindcss/ui/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   theme: {
     colors,
     extend: {},
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), //
+  ],
 }
